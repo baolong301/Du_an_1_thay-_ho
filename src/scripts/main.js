f371128e24741deba892d4f08395fa699a386743
baolong301 <lelacbinh3107@gmail.com>
2018-11-13 10:34:28 +0700

    fix animation body- Binh

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,37 @@
 // Main
-$(document).ready(function () {
-	// Phần Slider
-	$('.home-slider .owl-carousel').owlCarousel({
-		items: 1,
-		nav: false,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		loop:true,
-	});
-	// Phần Clients
-	$('.home_service .owl-carousel').owlCarousel({
-		items: 1,
-		nav: true,
-		dots: false,
-		loop:true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-	});
+$(document).ready(function() {
+    // Phần Slider
+    $('.home-slider .owl-carousel').owlCarousel({
+        items: 1,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        loop: true,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+        loop: true,
+    });
+    // Phần Clients
+    $('.home_service .owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+        responsive: {
+            480: {
+                nav: false,
+            },
+            760: {
+                nav: true
+            },
+            992: {
+                nav: true
+            }
+
+        }
+    });
 
 });
