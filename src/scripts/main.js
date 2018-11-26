ad1e866a57bf99a3392d26b7ee8e6a884bd42134
sideptraihihi <sitrandeptrai@gmail.com>
2018-11-26 19:57:05 +0700

    abc

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 // Main
 $(document).ready(function() {
+	var quantitiy=0;
+   $('.quantity-right-plus').click(function(e){
+        
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+            
+            $('#quantity').val(quantity + 1);
+
+          
+            // Increment
+        
+    });
+
+     $('.quantity-left-minus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+      
+            // Increment
+            if(quantity>0){
+            $('#quantity').val(quantity - 1);
+            }
+    });
+
     // Phần Slider
     $('.home-slider .owl-carousel').owlCarousel({
         items: 1,
